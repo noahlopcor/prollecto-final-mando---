52c5c972ada14aaef215a14c7d1f093df12d932e
noahlopcor <99145902+noahlopcor@users.noreply.github.com>
2022-04-06 09:30:11 +0200

    jwt h

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
@@ -5,3 +14,13 @@ input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
 })
 radio.setGroup(5)
+music.playMelody("C5 B A G F E D C ", 120)
+basic.showString("hola")
+basic.forever(function () {
+    radio.sendNumber(8)
+    if (input.isGesture(Gesture.TiltRight)) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+    }
+})
